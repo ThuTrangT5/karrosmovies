fd641f2a8b40aa972977e777e16fa7c9ab6f25ba
TrangNguyen <thutrangitmt@gmail.com>
2020-04-18 13:24:20 +0700

    - init

---
+++ KarrosMovies/KarrosMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KarrosMovies
+//
+//  Created by ThuTrangT5 on 4/17/20.
+//  Copyright © 2020 ThuTrangT5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
